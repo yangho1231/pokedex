a1ff808e29b2980f8a16f3f4e6d4135d44e1bcd5
yangho1231 <yangho1231@gmail.com>
2016-11-07 16:52:28 -0700

    Fixed error when refreshing from other link and added zoom effect

--- js/directive/pokeDirective.js
+++ js/directive/pokeDirective.js
@@ -1,7 +1,10 @@
 angular.module('pokemonApp').directive('pokemonDirective', function() {
   return {
     restrict: 'EA',
-    templateUrl: 'https://raw.githubusercontent.com/yangho1231/pokedex/gh-pages/views/home.html',
+    template:`<section class='home-main'> <div id='home-image' class='home-image'> </div><input class='search-box' ng-model='filterName.name' placeholder="Type Here"></input> <select ng-model='sortBy'> <option>------------</option> <option value='name'>A-Z</option> <option value='-name'>Z-A</option> <option value='id'>Lowest Number</option> <option value='-id'>Highest Number</option> </select> <section class='home-intro'> <div ng-repeat="data in pokemon | filter: filterName | orderBy: sortBy"> <div ui-sref='info({id: data.name})' ng-click="getIndividual($index + 1)"> <div class='wrapper'> <img class='img-list' ng-src='{{data.img}}'></img> </div><div>{{data.name}}</div><div>#{{data.id}}</div></div></div></section>`
+
+
+    //  'https://raw.githubusercontent.com/yangho1231/pokedex/gh-pages/views/home.html',
     // ../../views/home.html
   };
 
